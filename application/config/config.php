28052c8d74d069c65cb17d97cdc7873ecef07029
Rudy Eko Prasetya <rudyekoprasetya@yahoo.co.id>
2021-08-25 20:11:24 +0700

    [ADD] dynamic URL base url

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,10 @@ date_default_timezone_set('Asia/Jakarta');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/plat-ag';
+// $config['base_url'] = 'http://localhost/plat-ag';
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 
 /*
 |--------------------------------------------------------------------------
